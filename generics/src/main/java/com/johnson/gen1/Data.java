eff9c2a9955fea1834dbc75a6ed3b50b693e6d59
Johnson <johnson.arrobo@gmail.com>
2022-11-23 12:46:43 +0100

    Creada clase casi genérica con atributos de tipo Object

--- generics/src/main/java/com/johnson/gen1/Data.java
+++ generics/src/main/java/com/johnson/gen1/Data.java
@@ -1,16 +1,16 @@
 package com.johnson.gen1;
 
 public class Data {
-    private String data;
-    public Data(String data) {
+    private Object data;
+    public Data(Object data) {
         this.data = data;
     }
 
-    public String getData() {
+    public Object getData() {
         return data;
     }
 
-    public void setData(String data) {
+    public void setData(Object data) {
         this.data = data;
     }
 
--- generics/src/main/java/com/johnson/gen1/Main.java
+++ generics/src/main/java/com/johnson/gen1/Main.java
@@ -2,7 +2,16 @@ package com.johnson.gen1;
 
 public class Main {
     public static void main(String[] args) {
-        Data d1 = new Data("data1");
-        System.out.println(d1 + "\nof type:" + d1.getData().getClass());
+
+        // funciona correctamente con tipo String
+        Data dataString = new Data("hola mundo!");
+        System.out.println(dataString + "\nof type:" + dataString.getData().getClass());
+
+        // funciona correctamente con cualquier tipo de datos
+        Data dataInt = new Data(5);
+        System.out.println(dataInt + "\nof type:" + dataInt.getData().getClass());
+
+        // intento downcast del atributo data (tipo Object) de dataString a tipo entero
+        int valor = (int) dataString.getData(); // error aunque el compiler no detecta nada
     }
 }
