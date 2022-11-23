a8763ec445c3ee89ce19c1923046a6d59fa5ff26
Johnson <johnson.arrobo@gmail.com>
2022-11-23 12:36:31 +0100

    Creada clase con atributos de tipado específico (String)

---
+++ generics/src/main/java/com/johnson/gen1/Main.java
@@ -0,0 +1,8 @@
+package com.johnson.gen1;
+
+public class Main {
+    public static void main(String[] args) {
+        Data d1 = new Data("data1");
+        System.out.println(d1 + "\nof type:" + d1.getData().getClass());
+    }
+}
